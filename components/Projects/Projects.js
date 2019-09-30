6dff2935797d94ff4374b7f130ac6c93c95c5bc6
Yaolegol <m160160@yandex.ru>
2019-10-01 02:52:51 +0700

    extend layout

---
+++ components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Projects.scss'
+
+const Projects = () => (
+  <div className='Projects'>
+    
+  </div>
+)
+
+export default Projects
--- components/main.js
+++ components/main.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import About from '../components/About/About'
 import Menu from '../components/Menu/Menu'
+import Projects from '../components/Projects/Projects'
 import './main.scss'
 
 const main = () => (
   <div className='Main' >
     <About />
     <Menu />
+    <Projects />
   </div>
 )
 
