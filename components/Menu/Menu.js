512518271bcf8734757e159a6fb60bfd5c28a9bc
Yaolegol <m160160@yandex.ru>
2019-10-01 23:39:47 +0700

    menu section done

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 import './Menu.scss'
+import ProjectsImg from './img/ProjectsImg.png'
+import AboutImg from './img/AboutImg.png'
 
 const Menu = () => (
   <div className='Menu'>
-    
+    <div className='Item'>
+      <img src={AboutImg} alt="About Img" />
+      <p>Обо мне</p>
+    </div>
+    <div className='Item'>
+      <img src={ProjectsImg} alt="Projects Img" />
+      <p>Мои проекты</p>
+    </div>
   </div>
 )
 
