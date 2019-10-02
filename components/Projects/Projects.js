f4dafa18342ad6f52393a06adb2af7886b12778f
Yaolegol <m160160@yandex.ru>
2019-10-02 21:22:50 +0700

    takeoff section done

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -4,6 +4,7 @@ import Carousel from 'react-bootstrap/Carousel'
 import './Projects.scss'
 import TakeoffImg from './img/Takeoff_img.jpg'
 import VeltryImg from './img/Veltry_img.jpg'
+import GithubImg from './img/Github_icon.png'
 
 const defaultProps = {
   interval: null
@@ -13,9 +14,10 @@ Carousel.defaultProps = defaultProps;
 
 const Projects = () => (
   <div className='Projects'>
-    <div className='Projects-CarouselWrap'>
-      <Carousel>
-        <Carousel.Item>
+
+    <Carousel>
+      <Carousel.Item>
+        <div className='Projects-CarouselWrap'>
           <a href="http://takeoff-rename.herokuapp.com/" target='_blank'>
             <img
               className="d-block w-100"
@@ -23,13 +25,31 @@ const Projects = () => (
               alt="First slide"
             />
           </a>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+        </div>
+        <Carousel.Caption>
+          <a href="http://takeoff-rename.herokuapp.com/" target='_blank'>
+            <h3>Takeoff</h3>
+            <h6>(landing page)</h6>
+          </a>
+          <h5>Особенности проекта</h5>
+          <p>- выполнен на чистых HTML, CSS + немного jQuery</p>
+          <p>- верстка выполнена по БЭМ</p>
+          <p>- карусель - Owl Carousel 2 с добавлением скрипта на кнопки влево-вправо</p>
+          <p>- навигация с использованием якорей</p>
+          <p>- backend на node.js (работает кнопка "contact" в правом верхнем углу)</p>
+          <a href="https://github.com/Yaolegol/TAKEOFF" target='_blank'>
+            <img
+              className='GitHubImg'
+              src={GithubImg}
+              alt="GitHub"
+            />
+            <p>Посмотреть код</p>
+          </a>
+        </Carousel.Caption>
 
-        </Carousel.Item>
-        <Carousel.Item>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='Projects-CarouselWrap'>
           <a href="https://veltry.herokuapp.com/" target='_blank'>
             <img
               className="d-block w-100"
@@ -37,16 +57,31 @@ const Projects = () => (
               alt="Third slide"
             />
           </a>
-        </Carousel.Item>
-        <Carousel.Item>
+        </div>
+        <Carousel.Caption>
+          <a href="http://takeoff-rename.herokuapp.com/" target='_blank'>
+            <h3>Veltry</h3>
+            <h6>(landing page)</h6>
+          </a>
+          <h5>Особенности проекта</h5>
+          <p>- выполнен на чистых HTML, CSS + немного jQuery</p>
+          <p>- верстка выполнена по БЭМ</p>
+          <p>- карусель - Owl Carousel 2 с добавлением скрипта на кнопки влево-вправо</p>
+          <p>- навигация с использованием якорей</p>
+          <p>- backend на node.js (работает кнопка "contact" в правом верхнем углу)</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='Projects-CarouselWrap'>
           <img
             className="d-block w-100"
             src={VeltryImg}
             alt="Third slide"
           />
-        </Carousel.Item>
-      </Carousel>
-    </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+
   </div>
 )
 
