1ad55945021c529bd2b9ac1a7c5db6028e8b0b56
Yaolegol <m160160@yandex.ru>
2019-10-02 19:03:36 +0700

    add carousel items

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -2,46 +2,51 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 
 import './Projects.scss'
+import TakeoffImg from './img/Takeoff_img.jpg'
+import VeltryImg from './img/Veltry_img.jpg'
+
+const defaultProps = {
+  interval: null
+};
+
+Carousel.defaultProps = defaultProps;
 
 const Projects = () => (
   <div className='Projects'>
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Third slide"
-        />
+    <div className='Projects-CarouselWrap'>
+      <Carousel>
+        <Carousel.Item>
+          <a href="http://takeoff-rename.herokuapp.com/" target='_blank'>
+            <img
+              className="d-block w-100"
+              src={TakeoffImg}
+              alt="First slide"
+            />
+          </a>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="https://veltry.herokuapp.com/" target='_blank'>
+            <img
+              className="d-block w-100"
+              src={VeltryImg}
+              alt="Third slide"
+            />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={VeltryImg}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   </div>
 )
 
