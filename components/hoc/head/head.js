aab2948aae3948b3453d0e59b20aef9aa90a0d10
Yaolegol <m160160@yandex.ru>
2019-10-04 17:23:16 +0700

    remove attribute importance

--- components/hoc/head/head.js
+++ components/hoc/head/head.js
@@ -4,7 +4,7 @@ import Head from 'next/head'
 const HeadHOC = () => (
     <Head>
         <title>Home</title>
-        <link rel='icon' href='/static/favicon.ico' importance='low' />
+        <link rel='icon' href='/static/favicon.ico'/>
         <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
