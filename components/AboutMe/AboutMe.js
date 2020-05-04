df5afd0921676d2102e3ef120378a5326b3c017e
Oleg <o.oleynik@dev.userstory.ru>
2020-05-04 23:46:32 +0700

    add grid

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -4,51 +4,59 @@ import './AboutMe.scss'
 
 const AboutMe = () => (
   <div className='AboutMe'>
-    <h1>Обо мне</h1>
+      <h1>Обо мне</h1>
 
-    <table>
-      <tbody>
-        <tr>
-          <th scope="row">Возраст</th>
-          <td>31 год</td>
-        </tr>
-        <tr>
-          <th scope="row">Город</th>
-          <td>Томск</td>
-        </tr>
-        <tr>
-          <th scope="row">Образование</th>
-          <td>Высшее (ЮИ ТГУ, очная форма обучения)</td>
-        </tr>
-        <tr>
-          <th scope="row">Работа</th>
-          <td>Frontend разработчик (с 2019 по настоящее время)</td>
-          <td>Юрисконсульт (с 2012 по 2019 года)</td>
-        </tr>
-        <tr>
-          <th scope="row">Интересы</th>
-          <td>Frontend (преимущественно), backend, mobile development</td>
-        </tr>
-        <tr>
-          <th scope="row">Что изучил?</th>
-          <td>HTML, CSS, БЭМ, Sass, less, адаптивная верстка, js, jQuery, react, react-router, redux, redux-promise, redux-thunk, reselect, classnames, next, axios, ajax, fetch, promise, async-await, pug, ejs, handlebars, webpack, gulp, material-ui, bootstrap, git, jira, jest, mocha, node, express, multer, JWT, mongodb, mongoose, sql, php, c#, ооп, REST API, GraphQL, React Native, Flutter, docker, Angular, Vue</td>
-        </tr>
-        <tr>
-          <th scope="row">Технологии, которые использовал в production:</th>
-          <td>HTML, CSS, БЭМ, less, адаптивная верстка, js, react, react-router, redux, redux-thunk, reselect, classnames, fetch, promise, async-await, webpack, git, ооп, REST API, React Native, Flutter, docker</td>
-        </tr>
-        <tr>
-          <th scope="row">Что делал в качестве frontend разработчика?</th>
-          <td>- создавал компоненты (верстка, интеграция с backend) разного уровня сложности для сайта на react с 0</td>
-          <td>- верстал страницы с использованием HTML и less для сайта</td>
-          <td>- делал интеграцию с backend на чистом js для сайта</td>
-          <td>- создавал компоненты (верстка, интеграция с backend) на React Native</td>
-          <td>- создавал компоненты (верстка, интеграция с backend) на Flutter</td>
-          <td>- правил баги в существующих проектах</td>
-        </tr>
-      </tbody>
-    </table>
+      <div className='AboutMe__Info'>
+          <div className='AboutMe__Info__TitleAria'>Возраст</div>
+          <div className='AboutMe__Info__DescriptionAria'>31 год</div>
 
+          <div className='AboutMe__Info__TitleAria'>Город</div>
+          <div className='AboutMe__Info__DescriptionAria'>Томск</div>
+
+          <div className='AboutMe__Info__TitleAria'>Образование</div>
+          <div className='AboutMe__Info__DescriptionAria'>Высшее (ЮИ ТГУ, очная форма обучения)</div>
+
+          <div className='AboutMe__Info__TitleAria'>Работа</div>
+          <div>
+              <div className='AboutMe__Info__DescriptionAria'>- Frontend разработчик (с 2019 по настоящее время),</div>
+              <div className='AboutMe__Info__DescriptionAria'>- Юрисконсульт (с 2012 по 2019 года)</div>
+          </div>
+
+          <div className='AboutMe__Info__TitleAria'>Интересы</div>
+          <div className='AboutMe__Info__DescriptionAria'>Frontend (преимущественно), backend, mobile development</div>
+
+          <div className='AboutMe__Info__TitleAria'>Что изучил?</div>
+          <div className='AboutMe__Info__DescriptionAria'>HTML, CSS, БЭМ, Sass, less, адаптивная верстка, js, jQuery,
+              react, react-router, redux, redux-promise,
+              redux-divunk, reselect, classnames, next, axios, ajax, fetch, promise, async-await, pug, ejs, handlebars,
+              webpack, gulp, material-ui, bootstrap, git, jira, jest, mocha, node, express, multer, JWT, mongodb,
+              mongoose, sql, php, c#, ООП, REST API, GraphQL, React Native, Flutter, docker, Angular, Vue, Figma,
+              Ubuntu, MacOS, Visual Studio, VSCode, PHPStorm
+          </div>
+
+          <div className='AboutMe__Info__TitleAria'>Технологии, которые использовал в production:</div>
+          <div className='AboutMe__Info__DescriptionAria'>HTML, CSS, less, адаптивная верстка, js, react, react-router,
+              redux, redux-divunk, reselect,
+              classnames, fetch, promise, async-await, webpack, git, REST API, React Native, Flutter, docker, Figma,
+              Ubuntu, MacOS, PHPStorm
+          </div>
+
+          <div className='AboutMe__Info__TitleAria'>Что делал в роли frontend разработчика?</div>
+          <div>
+              <div className='AboutMe__Info__DescriptionAria'>- Создавал компоненты (верстка, интеграция с backend)
+                  разного уровня сложности на react.js,
+              </div>
+              <div className='AboutMe__Info__DescriptionAria'>- Верстал страницы с использованием HTML и less,</div>
+              <div className='AboutMe__Info__DescriptionAria'>- Интегрировал приложение с backend`ом на чистом js,</div>
+              <div className='AboutMe__Info__DescriptionAria'>- Создавал компоненты (верстка, интеграция с backend) на
+                  React Native,
+              </div>
+              <div className='AboutMe__Info__DescriptionAria'>- Создавал компоненты (верстка, интеграция с backend) на
+                  Flutter,
+              </div>
+              <div className='AboutMe__Info__DescriptionAria'>- Правил баги в существующих проектах</div>
+          </div>
+      </div>
   </div>
 )
 
