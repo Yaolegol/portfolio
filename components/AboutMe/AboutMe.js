f78bfe8090f1735dd45f3fb8c78c9ef11fda2983
Oleg <o.oleynik@dev.userstory.ru>
2020-05-04 21:40:44 +0700

    fix rows

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -32,8 +32,12 @@ const AboutMe = () => (
         <tr>
           <th scope="row">Что изучил?</th>
           <td>HTML, CSS, БЭМ, Sass, less, адаптивная верстка, js, jQuery, react, react-router, redux, redux-promise, redux-thunk, reselect, classnames, next, axios, ajax, fetch, promise, async-await, pug, ejs, handlebars, webpack, gulp, material-ui, bootstrap, git, jira, jest, mocha, node, express, multer, JWT, mongodb, mongoose, sql, php, c#, ооп, REST API, GraphQL, React Native, Flutter, docker, Angular, Vue</td>
+        </tr>
+        <tr>
           <th scope="row">Технологии, которые использовал в production:</th>
           <td>HTML, CSS, БЭМ, less, адаптивная верстка, js, react, react-router, redux, redux-thunk, reselect, classnames, fetch, promise, async-await, webpack, git, ооп, REST API, React Native, Flutter, docker</td>
+        </tr>
+        <tr>
           <th scope="row">Что делал в качестве frontend разработчика?</th>
           <td>- создавал компоненты (верстка, интеграция с backend) разного уровня сложности для сайта на react с 0</td>
           <td>- верстал страницы с использованием HTML и less для сайта</td>
