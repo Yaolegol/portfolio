ebdc75665f7045e7e2a5517af0628878fc3df3d0
Oleg <o.oleynik@dev.userstory.ru>
2020-05-04 21:33:26 +0700

    fix build problem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 
 import HeadHOC from '../components/hoc/head/head'
-import Main from '../components/Main'
+import Main from '../components/main'
 
 const Home = () => (
   <Fragment>
