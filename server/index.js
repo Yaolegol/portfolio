4c0c92aa58fe2f9fe7b2c3d54babce24a9236860
Oleg <m160160@yandex.ru>
2021-01-21 21:43:24 +0700

    fix server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path')
+
+const projectRootPath = path.resolve(__dirname, '../')
 
 const app = express();
-app.use(express.static('../dist'));
+app.use(express.static(path.resolve(projectRootPath, 'dist')))
 
-app.listen(process.env.PORT || 8080, () => console.log('!!!!!!!!!!!Server start at localhost:8080!!!!!!!!!!!'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('dist','index.html'))
+})
+
+app.listen(process.env.PORT || 8000, () => console.log('!!!!!!!!!!!Server start at localhost:8000!!!!!!!!!!!'));
