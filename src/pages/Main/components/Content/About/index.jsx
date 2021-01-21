7675bf87f9d5f47c50cf81eb5f542e4c9c723188
Oleg <m160160@yandex.ru>
2021-01-21 19:55:05 +0700

    add about data

--- src/pages/Main/components/Content/About/index.jsx
+++ src/pages/Main/components/Content/About/index.jsx
@@ -1,11 +1,31 @@
 // @flow
-import React from "react";
+import aboutList from "config/about.json";
+import { getRandomString } from "common/helpers/random";
+import React, { useMemo } from "react";
 import "./index.less";
 
 export const About = (): React$Node => {
+    const content = useMemo(() => {
+        return aboutList.map(({ description, title }) => {
+            const _description = description.map((des) => {
+                return <div key={getRandomString()}>{des}</div>;
+            });
+            return (
+                <div className="main-page-content-about__item" key={title}>
+                    <div className="main-page-content-about__item-title">
+                        <strong>{title}</strong>
+                    </div>
+                    <div className="main-page-content-about__item-description">
+                        {_description}
+                    </div>
+                </div>
+            );
+        });
+    }, []);
+
     return (
         <div className="main-page-content-about">
-            about
+            {content}
         </div>
     );
 };
