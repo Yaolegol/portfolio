fa56fe0a64d7fbc02cd83d66a995de42fc189b6c
Oleg <m160160@yandex.ru>
2021-01-23 00:42:02 +0700

    style slider controls

--- src/pages/Main/components/Content/Projects/Slider/index.jsx
+++ src/pages/Main/components/Content/Projects/Slider/index.jsx
@@ -4,13 +4,10 @@ import ArrowIcon from "images/arrow-right.svg";
 import { Slide } from "pages/Main/components/Content/Projects/Slider/Slide";
 import projectsList from "config/projects.json";
 import React, { useCallback, useMemo, useState } from "react";
-import { useIntl } from "react-intl";
 import "./index.less";
 
 export const Slider = (): React$Node => {
     const [swiper, setSwiper] = useState();
-
-    const intl = useIntl();
 
     const content = useMemo(() => {
         return projectsList.map(
@@ -55,11 +52,17 @@ export const Slider = (): React$Node => {
                     onClick={slidePrev}
                 >
                     <ArrowIcon className="project-slider__control-icon project-slider__control-icon_left" />
+                    <span className="project-slider__controls-description project-slider__controls-description_prev">
+                        prev project
+                    </span>
                 </button>
                 <button
                     className="project-slider__control-button"
                     onClick={slideNext}
                 >
+                    <span className="project-slider__controls-description project-slider__controls-description_next">
+                        next project
+                    </span>
                     <ArrowIcon className="project-slider__control-icon" />
                 </button>
             </div>
