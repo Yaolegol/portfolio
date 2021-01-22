bfb8ba2bdac4caf56aa1e4565b8ed78292cac70f
Oleg <m160160@yandex.ru>
2021-01-22 20:51:53 +0700

    add contacts data and linkedin profile

--- src/pages/Main/components/Header/index.jsx
+++ src/pages/Main/components/Header/index.jsx
@@ -1,10 +1,52 @@
 // @flow
+import contactsData from "config/contacts.json";
 import { LangSelect } from "modules/Locale/components/LangSelect";
-import React from "react";
+import { selectCurrentLocale } from "modules/Locale/selectors";
+import React, { useMemo } from "react";
 import { FormattedMessage } from "react-intl";
+import { useSelector } from "react-redux";
 import "./index.less";
 
+const { email, phone } = contactsData;
+
 export const Header = (): React$Node => {
+    const currentLocale = useSelector(selectCurrentLocale);
+
+    const socialLinks = useMemo(() => {
+        if (currentLocale === "ru") {
+            return (
+                <div className="main-page-header__socials-item">
+                    <a
+                        href="https://vk.com/id41167330"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <img
+                            alt="Vk"
+                            className="main-page-header__socials-icon"
+                            src="images/vk.png"
+                        />
+                    </a>
+                </div>
+            );
+        }
+        return (
+            <div className="main-page-header__socials-item">
+                <a
+                    href="https://www.linkedin.com/in/oleg-oleinik-160160"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    <img
+                        alt="LinkedIn"
+                        className="main-page-header__socials-icon"
+                        src="images/linkedin.svg"
+                    />
+                </a>
+            </div>
+        )
+    }, [currentLocale]);
+
     return (
         <div className="main-page-header">
             <img
@@ -29,33 +71,19 @@ export const Header = (): React$Node => {
             <div className="main-page-header__contacts-section">
                 <div className="main-page-header__contacts-item">
                     <b>
-                        <a href="tel:+79069473139">
-                            {<FormattedMessage id="main.phone" />}: +79069473139
+                        <a href={`tel:${phone}`}>
+                            {<FormattedMessage id="main.phone" />}: {phone}
                         </a>
                     </b>
                 </div>
                 <div className="main-page-header__contacts-item">
                     <b>
-                        <a href="mailto:web160160@gmail.com">
-                            Email: web160160@gmail.com
-                        </a>
+                        <a href={`mailto:${email}`}>Email: {email}</a>
                     </b>
                 </div>
             </div>
             <div className="main-page-header__socials-section">
-                <div className="main-page-header__socials-item">
-                    <a
-                        href="https://vk.com/id41167330"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                    >
-                        <img
-                            alt="Vk"
-                            className="main-page-header__socials-icon"
-                            src="images/vk.png"
-                        />
-                    </a>
-                </div>
+                {socialLinks}
             </div>
         </div>
     );
