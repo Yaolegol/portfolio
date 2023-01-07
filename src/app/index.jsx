9d1f7164a4f9d3601c543a8e77f6f80d9e09cdf1
Oleg <m160160@yandex.ru>
2023-01-07 23:13:23 +0700

    refactor layout

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,16 @@
 // @flow
+import { Layout } from "common/components/Layout";
 import Home from "main/Home";
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import "./index.less";
 
 const App = () => {
-    return <Home />;
+    return (
+        <Layout>
+            <Home />
+        </Layout>
+    );
 };
 
 export default hot(App);
--- src/main/Home/index.jsx
+++ src/main/Home/index.jsx
@@ -1,5 +1,4 @@
 // @flow
-import { Layout } from "common/components/Layout";
 import { Content } from "main/Home/components/Content";
 import { Header } from "main/Home/components/Header";
 import React from "react";
@@ -7,12 +6,10 @@ import "./index.less";
 
 const Home = (): React$Node => {
     return (
-        <Layout>
-            <div className="main-page">
-                <Header />
-                <Content />
-            </div>
-        </Layout>
+        <div className="main-page">
+            <Header />
+            <Content />
+        </div>
     );
 };
 
