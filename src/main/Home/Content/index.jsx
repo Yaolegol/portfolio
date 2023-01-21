3d8acd3af5f5ebdf3e1fa2d68bfa1500ff3c92b0
Oleg <m160160@yandex.ru>
2023-01-21 20:54:31 +0700

    add icons

--- src/main/Home/Content/index.jsx
+++ src/main/Home/Content/index.jsx
@@ -28,46 +28,46 @@ export const Content = (): React$Node => {
     }, [contentId]);
 
     return (
-        <div className="main-page-content">
-            <div className="main-page-content__tabs-container">
+        <div className="home-page-content">
+            <div className="home-page-content__tabs-container">
                 <button
-                    className={cn("main-page-content__tabs-button", {
-                        "main-page-content__tabs-button_active":
+                    className={cn("home-page-content__tabs-button", {
+                        "home-page-content__tabs-button_active":
                             contentId === "about",
                     })}
                     onClick={changeContent("about")}
                 >
-                    <div className="main-page-content__tab">
+                    <div className="home-page-content__tab">
                         <img
                             alt="About me"
-                            className="main-page-content__tab-icon"
-                            src="images/people.png"
+                            className="home-page-content__tab-icon"
+                            src="/icons/profile.svg"
                         />
-                        <div className="main-page-content__tab-description">
+                        <div className="home-page-content__tab-description">
                             <FormattedMessage id="main.menu.about" />
                         </div>
                     </div>
                 </button>
                 <button
-                    className={cn("main-page-content__tabs-button", {
-                        "main-page-content__tabs-button_active":
+                    className={cn("home-page-content__tabs-button", {
+                        "home-page-content__tabs-button_active":
                             contentId === "projects",
                     })}
                     onClick={changeContent("projects")}
                 >
-                    <div className="main-page-content__tab">
+                    <div className="home-page-content__tab">
                         <img
                             alt="My projects"
-                            className="main-page-content__tab-icon"
-                            src="images/projects.png"
+                            className="home-page-content__tab-icon"
+                            src="/icons/projects.svg"
                         />
-                        <div className="main-page-content__tab-description">
+                        <div className="home-page-content__tab-description">
                             <FormattedMessage id="main.menu.projects" />
                         </div>
                     </div>
                 </button>
             </div>
-            <div className="main-page-content__content-section">{content}</div>
+            <div className="home-page-content__content-section">{content}</div>
         </div>
     );
 };
