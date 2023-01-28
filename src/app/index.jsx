d36f983f93be368f96f437ee59f65eb89293264b
Oleg <m160160@yandex.ru>
2023-01-28 22:39:46 +0700

    add detect user locale

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,21 @@
 // @flow
 import { Layout } from "modules/Layout";
+import { actionSetLocale } from "modules/Locale/actions";
 import Home from "main/Home";
-import React from "react";
+import React, { useEffect } from "react";
 import { hot } from "react-hot-loader/root";
+import { useDispatch } from "react-redux";
 import "./index.less";
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (window.navigator.language === "ru-RU") {
+            dispatch(actionSetLocale("ru"));
+        }
+    }, [dispatch]);
+
     return (
         <Layout>
             <Home />
