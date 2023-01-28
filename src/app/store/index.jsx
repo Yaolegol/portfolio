9c72119873bb2e98dc47b33b7a9ca0222c91f6de
Oleg <m160160@yandex.ru>
2023-01-28 22:29:13 +0700

    fix tabs

--- src/app/store/index.jsx
+++ src/app/store/index.jsx
@@ -1,8 +1,9 @@
 // @flow
 import { rootReducer } from "app/reducer";
 import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
 
-const middlewares = [];
+const middlewares = [thunk];
 const reduxDevTools =
     process.env.NODE_ENV === "development" &&
     window.__REDUX_DEVTOOLS_EXTENSION__
